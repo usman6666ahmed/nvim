e4cd3744b008d3ce1e26d37e46de22ff8d1d79c0
usman6666ahmed <usman.imtiaz@devsinc.com>
2022-07-26 22:31:41 +0500

    cmp

--- init.lua
+++ init.lua
@@ -2,4 +2,5 @@ require "user.keymaps"
 require "user.plugins"
 require "user.colors"
 require "user.options"
+require "user.cmp"
 require('telescope').setup{ defaults = { file_ignore_patterns = {".git", "node_modules"} } }
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,13 +56,20 @@ return packer.startup(function(use)
     -- Themes
   use "NLKNguyen/papercolor-theme"
 
-
-
   -- Vim Air Line
   use "vim-airline/vim-airline"
   use "vim-airline/vim-airline-themes"
 
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
 
+  -- Snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
 
 
